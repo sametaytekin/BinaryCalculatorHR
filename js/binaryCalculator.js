6f7a12961ff8f175077c48369b38c706975ce661
Samet Halit Aytekin <60233262+sametaytekin@users.noreply.github.com>
2022-09-04 12:43:41 +0300

    First Commit

---
+++ js/binaryCalculator.js
@@ -0,0 +1,57 @@
+let x= document.getElementById("btnClr")
+let buttons= document.getElementById("btns")
+let operator
+
+buttons.addEventListener("click",function(ev){
+    let action=document.getElementById(ev.target.id).innerHTML
+    if(ev.target.tagName=="BUTTON"){
+        switch(ev.target.id){
+            case "btn0":
+            case "btn1":
+                document.getElementById("res").innerHTML+=action
+                break
+            case "btnSum":
+            case "btnSub":
+            case "btnMul":
+            case "btnDiv":
+            document.getElementById("res").innerHTML+=action
+            operator=action
+            break    
+           case "btnClr":
+            document.getElementById("res").innerHTML=" "
+                break
+            case "btnEql":
+            document.getElementById("res").innerHTML=
+               Calculator(document.getElementById("res").innerHTML,operator)
+            break
+
+        }
+    }
+    
+})
+
+
+function Calculator(text,operator){
+    let numbers = text.split(operator)
+    let number1= parseInt(numbers[0],2)
+    let number2 = parseInt(numbers[1],2)
+    let result=0
+
+    switch (operator) {
+        case "+":
+            result= number1+number2
+            break;
+        case "-":
+            result=number1-number2
+            break
+        case "*":
+            result=number1*number2
+            break
+        case "/":
+            result=number1/number2        
+    }
+
+    return result.toString(2)
+
+}
+
